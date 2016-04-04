a22c4870fccc9c12bd2863d49bd4dc89f5b2cbee
Hannes Mehnert <hannes@mehnert.org>
2016-04-04 17:23:59 +0200

    filter_map / map_find (copied over from TLS)

--- common/utils.ml
+++ common/utils.ml
@@ -1,2 +1,16 @@
 
-(* general useful utilities missing from the stdlib, such as map_find, filter_map, .. *)
+(* general useful utilities missing from the stdlib *)
+
+let rec filter_map ~f = function
+  | []    -> []
+  | x::xs ->
+      match f x with
+      | None    ->       filter_map ~f xs
+      | Some x' -> x' :: filter_map ~f xs
+
+let rec map_find ~f = function
+  | []    -> None
+  | x::xs ->
+      match f x with
+      | None         -> map_find ~f xs
+      | Some _ as x' -> x'
