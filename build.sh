5d3fd10416a37d1ad22c91fa8d4fad18bc5a59a9
Hannes Mehnert <hannes@mehnert.org>
2016-04-14 18:22:29 +0100

    rename pfkey to lib, remove helper/pf_to_fd

--- build.sh
+++ build.sh
@@ -15,13 +15,13 @@ action ()
         default) action lwt ; action helper ;;
         helper) cc -Wall -Werror -std=c99 -O3 -o helper/pf_to_tcp helper/pf_to_tcp.c ;
                 cc -Wall -Werror -std=c99 -O3 -o helper/pf_to_fd helper/pf_to_fd.c ;;
-        lwt) action pfkey ; $OCAMLBUILD lwt/ike_lwt.native ;;
-        pfkey) $OCAMLBUILD ike.cmx ike.cmxa ;;
-        test) action pfkey ; $OCAMLBUILD rfctests.native ;;
+        lwt) action lib ; $OCAMLBUILD lwt/ike_lwt.native ;;
+        lib) $OCAMLBUILD ike.cmx ike.cmxa ;;
+        test) action lib ; $OCAMLBUILD rfctests.native ;;
         doc) shift
              $OCAMLBUILD -no-links $OCAMLDOCFLAGS doc/api.docdir/index.html
              cp doc/style.css $BUILDDIR/$DOCDIRFILE/style.css ;;
-        clean) $OCAMLBUILD -clean ; rm -rf _tests ; rm -f helper/pf_to_tcp ;;
+        clean) $OCAMLBUILD -clean ; rm -rf _tests ; rm -f helper/pf_to_tcp ; rm -f helper/pf_to_fd ;;
         *) $OCAMLBUILD $* ;;
     esac
 }
